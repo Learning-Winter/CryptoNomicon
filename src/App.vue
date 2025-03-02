f484cf65ed75448315f9ef3ddbfd6007301bf862
Koval Bohdan <bohdankoval3012@gmail.com>
2025-03-02 22:00:43 +0200

    Independent work completed, additional breakdown into components

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,10 @@
     <div v-else class="container">
       <add-ticker
         @add-ticker="add"
+        @add-ticker-hint="addCryptoHint"
+        :isCryptoData="isCryptoData"
         :disabled="tooManyTickersAdded"
+        :isEnableError="isEnableError"
       />
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
@@ -93,57 +96,24 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - {{ !getlistCourseTikerToBTC.includes(selectedTicker.name) ? 'USD' : 'BTC'}}
-        </h3>
-        <div 
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, idx) of normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10 h-48"
-            ref="graphElement"
-          ></div>
-        </div>
-        <button type="button" class="absolute top-0 right-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <add-graph 
+        v-if="selectedTicker" 
+        :selectedTicker="selectedTicker" 
+        :graph="graph"
+        @update-max-graph="updateMaxGraphElements"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { subscribeToTicker, unsubscribeFromTicker, loadCryptoBase, deleteTikerToBTC, listUndefinedTickers, listCourseTikerToBTC } from "./api.js";
+import AddGraph from './components/addGraph.vue';
 import AddTicker from './components/AddTicker.vue';
 
 
 export default {
-  components: { AddTicker },
+  components: { AddTicker, AddGraph },
   name: "App",
 
   data() {
@@ -162,7 +132,6 @@ export default {
       page: 1,
 
       isCryptoData: null,
-      isCryptoHints: null,
       isDataLoaded: false,
       isEnableError: false,
     };
@@ -178,23 +147,12 @@ export default {
       }
     });
 
-    // if (windowData.filter) {
-    //   this.filter = windowData.filter
-    // }
-
-    // if (windowData.page) {
-    //   this.page = windowData.page
-    // }
-
     const tickersData = localStorage.getItem("cryptonomicon-list");
     this.loadedCryptoData();
 
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
-      // subscribeToTicker('BTC', test => {
-      //   console.log(test, listUndefinedTickers);
-        
-      // })
+
       this.tickers.forEach(ticker => {
         subscribeToTicker(ticker.name, newPrice => 
           this.updateTicker(ticker.name, newPrice)
@@ -203,12 +161,6 @@ export default {
     }
 
     setInterval(this.updateTickers, 5000);
-  },
-  mounted() {
-    window.addEventListener('resize', this.calculatMaxGraphElements);
-  },
-  beforeUnmount() {
-    window.removeEventListener('resize', this.calculatMaxGraphElements);
   },
   computed: {
     startIndex(){
@@ -251,17 +203,12 @@ export default {
       return listCourseTikerToBTC
     },
     tooManyTickersAdded() {
-      return this.tickers.length > 3;
+      return this.tickers.length > 10;
     }
   },
   methods: {
-    calculatMaxGraphElements(){
-      if(!this.$refs.graph){
-        return
-      }
-      const areaGrapWidth = this.$refs.graph.clientWidth;
-      // const elementGrapWidth = this.$refs.graphElement['0'].clientWidth
-      this.maxGraphElements = areaGrapWidth / 38
+    updateMaxGraphElements(value){
+      this.maxGraphElements = value
     },
     formatPrice(price, name){
       let numberPrice = Number(price)
@@ -277,13 +224,10 @@ export default {
       : numberPrice.toPrecision(2)
     },
     add(ticker) {
-      // if (!ticker){
-      //   return
-      // }
-      // if (this.tickers.some((item) => item.name === ticker.toUpperCase())) {
-      //   this.isEnableError = true;
-      //   return;
-      // }
+      if (this.tickers.some((item) => item.name === ticker.toUpperCase())) {
+        this.isEnableError = true;
+        return;
+      }
       let currentTicker = {
         name: ticker.toUpperCase(),
         price: "-",
@@ -308,22 +252,12 @@ export default {
     select(ticker) {
       this.selectedTicker = ticker;
     },
-    // handlerInput() {
-    //   this.isEnableError = false;
-    //   if (!this.ticker || this.ticker === " ") {
-    //     this.isCryptoHints = null;
-    //     return;
-    //   }
-    //   let listValue = Object.values(JSON.parse(JSON.stringify(this.isCryptoData.Data)));
-    //   this.isCryptoHints = listValue
-    //     .filter((item) => item.Symbol.includes(this.ticker.toUpperCase()))
-    //     .splice(0, 4);
-    // },
-    // addCryptoHint(cryptoHint) {
-    //   this.isEnableError = false;
-    //   this.ticker = cryptoHint;
-    //   this.add();
-    // },
+    addCryptoHint(cryptoHint) {
+      this.isEnableError = false;
+      this.ticker = cryptoHint;
+      
+      this.add(cryptoHint);
+    },
     updateTicker(tickerName, price){
       this.tickers.filter(t => t.name === tickerName).forEach(t => 
         {
@@ -371,22 +305,6 @@ export default {
         `${window.location.pathname}?filter=${value.filter}&page=${value.page}`
       )
     },
-    getlistCourseTikerToBTC: {
-      handler(val) {
-        
-        if (val.length > 0){
-          // console.log(this.tickers[0].price);
-          // this.tickers[0].price = 0.0555
-          subscribeToTicker('BTC', priceBTC => {
-            this.rateBTC = priceBTC
-          })
-        } else {
-          unsubscribeFromTicker('BTC')
-        }
-      },
-      immediate: true, 
-      deep: true
-    }
   },
 };
 </script>
--- src/components/AddTicker.vue
+++ src/components/AddTicker.vue
@@ -9,16 +9,16 @@
               class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
               placeholder="Например DOGE" />
           </div>
-          <!-- <div v-if="this.isCryptoHints" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
-            <span v-for="(cryptoHint, inx) in this.isCryptoHints" @click="addCryptoHint(cryptoHint.Symbol)"
+          <div v-if="isCryptoHints" class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+            <span v-for="(cryptoHint, inx) in isCryptoHints" @click="addCryptoHint(cryptoHint.Symbol)"
               class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
               :key="inx">
               {{ cryptoHint.Symbol }}
             </span>
-          </div> -->
-          <!-- <div v-if="isEnableError" class="text-sm text-red-600">
+          </div>
+          <div v-if="isEnableError" class="text-sm text-red-600">
             Такой тикер уже добавлен
-          </div> -->
+          </div>
         </div>
       </div>
       <add-button 
@@ -39,14 +39,25 @@ export default {
       type: Boolean,
       required: false,
       default: false,
+    },
+    isEnableError: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    isCryptoData: {
+      type: Object,
+      required: false,
     }
   },
   emits: {
-    'add-ticker':  value => typeof value === "string" && value.length > 0
+    'add-ticker':  value => typeof value === "string" && value.length > 0,
+    'add-ticker-hint': value => typeof value === "string" && value.length > 0
   },
   data() {
     return {
       ticker: '',
+      isCryptoHints: null,
     }
   },
   methods: {
@@ -57,6 +68,19 @@ export default {
       this.$emit('add-ticker', this.ticker)
       this.ticker = ""
     },
+    handlerInput() {
+      if (!this.ticker || this.ticker === " ") {
+        this.isCryptoHints = null;
+        return;
+      }
+      let listValue = Object.values(JSON.parse(JSON.stringify(this.isCryptoData.Data)));
+      this.isCryptoHints = listValue
+        .filter((item) => item.Symbol.includes(this.ticker.toUpperCase()))
+        .splice(0, 4);
+    },
+    addCryptoHint(tickerName){
+      this.$emit('add-ticker-hint', tickerName)
+    }
   },
 }
 </script>
